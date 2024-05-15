d69cc1aaca4f535f022a86576de27195184b2051
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2024-05-15 23:10:37 +0200

    Add basic domain entities

---
+++ Backend/Domain/Entities/WebsiteRecord.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class WebsiteRecord : IEntity
+    {
+        public Guid Id { get; set; }
+        // todo types might change, lets see, how database will handle it
+        public Uri Url { get; set; }
+        public Regex BoundaryRegexp { get; set; }
+        public TimeSpan Periodicity { get; set; }
+        public string Label { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+}
---
+++ Backend/Domain/Interfaces/Repositories/IWebsiteRecordRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IWebsiteRecordRepository
+    {
+        public Task<List<WebsiteRecord>> GetAllAsync();
+        public Task<WebsiteRecord?> GetByIdAsync(Guid id);
+        public Task<WebsiteRecord> CreateAsync(WebsiteRecord category);
+        public Task<WebsiteRecord?> UpdateAsync(Guid id, WebsiteRecord category);
+        public Task<WebsiteRecord?> DeleteAsync(Guid id);
+    }
+}
