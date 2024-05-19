0689b6c2d89a7c8166e5581320e7412a35a5884c
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2024-05-19 14:26:26 +0200

    Updated dtos with simpler types

--- Backend/Api/ApiDtos/WebsiteRecord/UpdateWebsiteRecordRequest.cs
+++ Backend/Api/ApiDtos/WebsiteRecord/UpdateWebsiteRecordRequest.cs
@@ -5,9 +5,9 @@ namespace Api.ApiDtos.WebsiteRecord
 {
     public class UpdateWebsiteRecordRequest
     {
-        public Uri Url { get; set; }
-        public Regex BoundaryRegexp { get; set; }
-        public TimeSpan Periodicity { get; set; }
+        public string Url { get; set; }
+        public string BoundaryRegexp { get; set; }
+        public int Periodicity { get; set; }
         public string Label { get; set; }
         public List<TagDto> Tags { get; set; }
     }
--- Backend/Api/ApiDtos/WebsiteRecord/WebsiteRecordDto.cs
+++ Backend/Api/ApiDtos/WebsiteRecord/WebsiteRecordDto.cs
@@ -7,9 +7,9 @@ namespace Api.ApiDtos.WebsiteRecord
     {
         public Guid Id { get; set; }
         // todo types might change, lets see, how database will handle it
-        public Uri Url { get; set; }
-        public Regex BoundaryRegexp { get; set; }
-        public TimeSpan Periodicity { get; set; }
+        public string Url { get; set; }
+        public string BoundaryRegexp { get; set; }
+        public int Periodicity { get; set; }
         public string Label { get; set; }
         public List<TagDto> Tags { get; set; }
     }
